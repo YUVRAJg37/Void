d87ab8a1c8887346f80e9330034be776a246c822
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2023-04-03 01:04:48 +0530

    Player System Implemented

---
+++ Source/Void/VoidGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoidGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOID_API AVoidGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
